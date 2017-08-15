cde2f915f45be553f8e43ab297f19f3ac0ae19f8
prasun <prasunanand.bitsp@gmail.com>
2017-08-15 10:52:38 +0530

    generate plots

--- public/script.js
+++ public/script.js
@@ -1,25 +1,86 @@
 var app = angular.module('resultApp',[]);
-
+var nmatrix_ruby, nmatrix_lapacke, nmatrix_jruby, arrayfire, arrayfire_lapacke;
 app.controller('MainCtrl', function($scope, $http) {
 
-  resultRubyUrl = "ruby.json"
-  resultJavaRubyUrl = "jruby.json"
-  resultLapackeRubyUrl = "ruby-lapacke.json"
-
+  NmatrixUrl = "nmatrix-ruby.json"
+  // NMatrixJRubyUrl = "nmatrix-jruby.json"
+  NMatrixLapackeRubyUrl = "nmatrix-lapacke.json"
+  ArrayFireUrl = "arrayfire.json"
+  ArrayFireLapackeUrl = "arrayfire-lapacke.json"
 
   $scope.checked= false;
-  $http.get(resultRubyUrl).success(function(data) {
-     $http.get(resultJavaRubyUrl).success(function(data2) {
-      $http.get(resultLapackeRubyUrl).success(function(data3) {
-        $scope.charts = data.features;
-        data.features.jruby = data2.features.jruby
-        data.features.rubyLapacke = data3.features.ruby
-        console.log($scope.charts);
 
-        for (key in $scope.charts.jruby){
-          // console.log($scope.charts.features[key])
+
+
+  $http.get(NmatrixUrl).success(function(data) {
+    nmatrix_ruby = data;
+    console.log(data['subtraction']);
+    $http.get(NMatrixLapackeRubyUrl).success(function(data) {
+      nmatrix_lapacke = data;
+      $http.get(ArrayFireUrl).success(function(data) {
+        arrayfire = data;
+        $http.get(ArrayFireLapackeUrl).success(function(data) {
+          arrayfire_lapacke = data;
+
+          console.log($scope.arith);
+
+          $scope.arith = ["addition", "subtraction"];
+
+          for (var i=0, l=  $scope.arith.length; i<l; i++){
+            // console.log(key)
+            key = $scope.arith[i];
+            console.log(nmatrix_ruby);
+            $(function () {
+              $('#chart'+key).highcharts({
+                  type: 'line',
+                  title: {
+                    text: 'Matrix '+key,
+                    x: -20 //center
+                  },
+                  subtitle: {x: -20},
+                  xAxis: {
+                    title: {
+                      text: 'Number of elements in one Matrix'
+                    },
+                  },
+                  yAxis: {
+                    title: {
+                        text: 'Time (s)'
+                    },
+                    type: 'logarithmic',
+                    plotLines: [{
+                        value: 0,
+                        width: 1,
+                        color: '#808080'
+                    }]
+                  },
+                  tooltip: {
+                    valueSuffix: 's'
+                  },
+                  legend: {
+                    layout: 'vertical',
+                    align: 'right',
+                    verticalAlign: 'middle',
+                    borderWidth: 0
+                  },
+                  series: [{
+                    name: "NMatrix-Ruby",
+                    data: nmatrix_ruby[key]
+                  },{
+                  //   name: "jruby",
+                  //   data: nmatrix_jruby[key]
+                  // },{
+                    name: "ArrayFire",
+                    data: arrayfire[key]
+                  },
+                ]
+              });
+            });
+          }
+
+
           $(function () {
-            $('#chart'+key).highcharts({
+            $('#chartmat_mult').highcharts({
                 type: 'line',
                 title: {
                   text: 'Matrix '+key,
@@ -52,24 +113,89 @@ app.controller('MainCtrl', function($scope, $http) {
                   borderWidth: 0
                 },
                 series: [{
-                  name: "ruby",
-                  data: $scope.charts.ruby[key]
+                  name: "NMatrix-LAPACK<br>-Ruby",
+                  data: nmatrix_lapacke["mat_mult"]
                 },{
-                  name: "jruby",
-                  data: $scope.charts.jruby[key]
+                  name: "NMatrix-Ruby",
+                  data: nmatrix_ruby["mat_mult"]
                 },{
-                  name: "ruby-lapacke",
-                  data: $scope.charts.rubyLapacke[key]
+                //   name: "jruby",
+                //   data: nmatrix_jruby[key]
+                // },{
+                  name: "ArrayFire",
+                  data: arrayfire["mat_mult"]
                 },
               ]
             });
           });
-        }
+
+
+          $scope.lapack = ["determinant", "lu_factorization"];
+
+          for (var i=0, l=  $scope.lapack.length; i<l; i++){
+            // console.log(key)
+            key = $scope.lapack[i];
+            console.log(nmatrix_ruby);
+            $(function () {
+              $('#chart'+key).highcharts({
+                  type: 'line',
+                  title: {
+                    text: 'Matrix '+key,
+                    x: -20 //center
+                  },
+                  subtitle: {x: -20},
+                  xAxis: {
+                    title: {
+                      text: 'Number of elements in one Matrix'
+                    },
+                  },
+                  yAxis: {
+                    title: {
+                        text: 'Time (s)'
+                    },
+                    type: 'logarithmic',
+                    plotLines: [{
+                        value: 0,
+                        width: 1,
+                        color: '#808080'
+                    }]
+                  },
+                  tooltip: {
+                    valueSuffix: 's'
+                  },
+                  legend: {
+                    layout: 'vertical',
+                    align: 'right',
+                    verticalAlign: 'middle',
+                    borderWidth: 0
+                  },
+                  series: [{
+                    name: "NMatrix-LAPACK<br>-Ruby",
+                    data: nmatrix_lapacke[key]
+                  },{
+                  //   name: "jruby",
+                  //   data: nmatrix_jruby[key]
+                  // },{
+                    name: "ArrayFire",
+                    data: arrayfire_lapacke[key]
+                  },
+                ]
+              });
+            });
+          }
+
+        });
       });
     });
   });
+  // $http.get(NMatrixJRubyUrl).success(function(data) {
+  //   nmatrix_lapacke = data;
+  // });
+
+
+
+
+
+
 })
 
-
-resultRubyUrl = "ruby.json"
-resultJavaRubyUrl = "jruby.json"
