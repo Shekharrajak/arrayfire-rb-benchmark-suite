9b8c7b59dd4df1bce3cf6265ac92d6cdbf8e8764
prasun <prasunanand.bitsp@gmail.com>
2017-08-29 17:45:56 +0530

    modified view

--- public/script.js
+++ public/script.js
@@ -1,5 +1,5 @@
 var app = angular.module('resultApp',[]);
-var nmatrix_ruby, nmatrix_lapacke, nmatrix_jruby, arrayfire, arrayfire_lapacke;
+var nmatrix_ruby, nmatrix_lapacke, nmatrix_jruby, arrayfire, arrayfire_lapacke, arrayfire_interface;
 app.controller('MainCtrl', function($scope, $http) {
 
   NmatrixUrl = "nmatrix-ruby.json"
@@ -7,6 +7,7 @@ app.controller('MainCtrl', function($scope, $http) {
   NMatrixLapackeRubyUrl = "nmatrix-lapacke.json"
   ArrayFireUrl = "arrayfire.json"
   ArrayFireLapackeUrl = "arrayfire-lapacke.json"
+  ArrayFireInterfaceUrl = "interface.json"
 
   $scope.checked= false;
 
@@ -23,6 +24,8 @@ app.controller('MainCtrl', function($scope, $http) {
           arrayfire = data;
           $http.get(ArrayFireLapackeUrl).success(function(data) {
             arrayfire_lapacke = data;
+          $http.get(ArrayFireInterfaceUrl).success(function(data) {
+            arrayfire_interface = data;
 
             console.log($scope.arith);
 
@@ -85,7 +88,7 @@ app.controller('MainCtrl', function($scope, $http) {
               $('#chartmat_mult').highcharts({
                   type: 'line',
                   title: {
-                    text: 'Matrix '+key,
+                    text: 'Matrix Multiplication',
                     x: -20 //center
                   },
                   subtitle: {x: -20},
@@ -126,6 +129,52 @@ app.controller('MainCtrl', function($scope, $http) {
                   },{
                     name: "ArrayFire",
                     data: arrayfire["mat_mult"]
+                  },
+                ]
+              });
+            });
+
+            $(function () {
+              $('#chart_interface').highcharts({
+                  type: 'line',
+                  title: {
+                    text: 'NMatrix - ArrayFire Interface ',
+                    x: -20 //center
+                  },
+                  subtitle: {x: -20},
+                  xAxis: {
+                    title: {
+                      text: 'Number of elements in one Matrix'
+                    },
+                  },
+                  yAxis: {
+                    title: {
+                        text: 'Time (s)'
+                    },
+                    type: 'logarithmic',
+                    plotLines: [{
+                        value: 0,
+                        width: 1,
+                        color: '#808080'
+                    }]
+                  },
+                  tooltip: {
+                    valueSuffix: 's'
+                  },
+                  legend: {
+                    layout: 'vertical',
+                    align: 'right',
+                    verticalAlign: 'middle',
+                    borderWidth: 0
+                  },
+                  series: [{
+                    name: "NMatrix-BLAS-Ruby",
+                    data: arrayfire_interface["mat_mult_cpu"],
+                    color: 'red'
+                  },{
+                    name: "ArrayFire",
+                    data: arrayfire_interface["mat_mult_gpu"],
+                    color: 'orange'
                   },
                 ]
               });
@@ -186,6 +235,7 @@ app.controller('MainCtrl', function($scope, $http) {
               });
             }
 
+          });
           });
         });
       });
